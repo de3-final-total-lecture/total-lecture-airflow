b75fb1446bbba6ef8193f6b8ebc4c0f80a0d6955
김승훈 <eephus625@icloud.com>
2024-08-05 15:15:20 +0900

    fix: final s3 to rds operator

--- dags/custom/mysqlhook.py
+++ dags/custom/mysqlhook.py
@@ -2,9 +2,9 @@ from airflow.providers.mysql.hooks.mysql import MySqlHook
 
 
 class CustomMySqlHook(MySqlHook):
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     self.local_infile = kwargs.pop("local_infile", True)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.local_infile = kwargs.pop("local_infile", True)
 
     def bulk_insert(self, sql, parameters):
         """
@@ -29,28 +29,18 @@ class CustomMySqlHook(MySqlHook):
             cursor.close()
             conn.close()
 
-    # def bulk_load(self, table: str, tmp_file: str) -> None:
-    #     conn = self.get_conn()
-    #     cur = conn.cursor()
-    #     cur.execute(
-    #         """
-    #         LOAD DATA LOCAL INFILE '{tmp_file}'
-    #         IGNORE
-    #         INTO TABLE {table}
-    #         FIELDS TERMINATED BY ';'
-    #         IGNORE 1 LINES;
-    #         """.format(
-    #             tmp_file=tmp_file, table=table
-    #         )
-    #     )
-    #     conn.commit()
     def bulk_load(self, table: str, tmp_file: str) -> None:
-        """Load a tab-delimited file into a database table."""
         conn = self.get_conn()
         cur = conn.cursor()
         cur.execute(
-            f"LOAD DATA LOCAL INFILE %s INTO TABLE {table}",
-            (tmp_file,),
+            """
+            LOAD DATA LOCAL INFILE '{tmp_file}'
+            IGNORE
+            INTO TABLE {table}
+            FIELDS TERMINATED BY ';'
+            IGNORE 1 LINES;
+            """.format(
+                tmp_file=tmp_file, table=table
+            )
         )
         conn.commit()
-        conn.close()  # type: ignore[misc]
