17008c516ee497e007327a98f79fc35c008d060d
김승훈 <eephus625@icloud.com>
2024-08-15 22:53:19 +0900

    fix: s3 to rds 코드 수정

--- dags/custom/mysqlhook.py
+++ dags/custom/mysqlhook.py
@@ -51,8 +51,8 @@ class CustomMySqlHook(MySqlHook):
                 level = IFNULL(level, 'default_level'),
                 is_new = IF(LOWER(TRIM(@is_new)) = 'TRUE', 1, 0),
                 is_recommend = IF(LOWER(TRIM(@is_recommend)) = 'TRUE', 1, 0),
-                like_count = IFNULL(like_count, 0),
                 keyword = FROM_BASE64(@keyword),
+                like_count = IFNULL(like_count, 0),
                 created_at = IFNULL(NULLIF(@created_at, ''), NOW()),
                 updated_at = IFNULL(NULLIF(@updated_at, ''), NOW())
         """
